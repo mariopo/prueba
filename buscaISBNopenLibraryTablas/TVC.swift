0e24eadc779faf9b1f3498e3760ea0ff0d50ebcc
mariopo <porras.mario@gmail.com>
2016-07-03 18:34:23 -0600

    Tarea 3a Semana Curso Accediendo a la Nube con iOS versión 2

--- buscaISBNopenLibraryTablas/TVC.swift
+++ buscaISBNopenLibraryTablas/TVC.swift
@@ -6,11 +6,23 @@
 //  Copyright © 2016 MAPC. All rights reserved.
 //
 
+struct libros {
+    var isbn : String
+    var titulo : String
+    
+    init(isbn : String, titulo : String){
+        self.isbn = isbn
+        self.titulo = titulo
+    }
+}
+
 import UIKit
 
 class TVC: UITableViewController {
     
     var isbns = [String]()
+    var tupla = [libros]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +33,7 @@ class TVC: UITableViewController {
         let addButton = UIBarButtonItem(title:"Agrega",style: UIBarButtonItemStyle.Plain, target: self, action:#selector(self.nuevoISBN(_:)))
         self.navigationItem.rightBarButtonItem = addButton
         
-        self.title = "\"ISBNs\""
+        self.title = "\"LIBROS\""
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
 
         // Uncomment the following line to preserve selection between presentations
@@ -32,11 +44,17 @@ class TVC: UITableViewController {
     }
     
     @IBAction func nuevoISBN(sender: AnyObject) {
+        var titulol : String = ""
+        var librol = libros(isbn: "", titulo: "")
         let alert = UIAlertController(title: "ISBN nuevo", message: "Agrega un ISBN", preferredStyle: .Alert)
         
         let saveAction = UIAlertAction(title: "Guardar", style: .Default, handler: {(action:UIAlertAction) -> Void in
             let textField = alert.textFields!.first
             self.isbns.append(textField!.text!)
+            titulol = self.buscaTitulo(textField!.text!)
+            librol.isbn = textField!.text!
+            librol.titulo = titulol
+            self.tupla.append(librol)
             self.tableView.reloadData()
         })
         
@@ -49,6 +67,39 @@ class TVC: UITableViewController {
         alert.addAction(cancelAction)
         
         presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func buscaTitulo(isbnl:String) -> String{
+        var isbn_titulo : String = ""
+        let etiqueta1 = "ISBN:"
+        let dir: String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+    // --> buscaTitulo
+        let urls = dir + isbnl
+        let url = NSURL(string: urls)
+        let datos: NSData? = NSData(contentsOfURL: url!)
+        
+        if datos == nil{
+            let alert = UIAlertController(title: "Atención", message:"Hay problemas para atender su solicitud. Revisa tu conexión a internet", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Aceptar", style: .Default) { _ in })
+            self.presentViewController(alert, animated: true) { }
+        } else{
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dico1 = json as! NSDictionary
+                if dico1[etiqueta1 + isbnl] != nil {
+                    let dico2 = dico1[etiqueta1 + isbnl] as! NSDictionary
+                    if dico2["title"] != nil{
+                        isbn_titulo = dico2["title"] as! NSString as String
+                    }
+                }
+            }
+            catch _ {
+                
+            }
+        }
+    // <-- buscaTitulo
+        
+        return isbn_titulo
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,7 +124,7 @@ class TVC: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("Celda", forIndexPath: indexPath)
         
         // Configure the cell...
-        cell.textLabel?.text = self.isbns[indexPath.row]
+        cell.textLabel?.text = self.tupla[indexPath.row].titulo
         
         return cell
     }
@@ -122,7 +173,7 @@ class TVC: UITableViewController {
         let cc = segue.destinationViewController as! detalleVC
         
         let ip = self.tableView.indexPathForSelectedRow
-        cc.isbn1 = self.isbns[ip!.row]
+        cc.isbn1 = self.tupla[ip!.row].isbn
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
